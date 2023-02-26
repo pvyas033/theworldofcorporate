b01d542d30a1d0c3c328372927ca7bbfeae134d1
Pankaj Vyas <pvyas@opentext.com>
2023-02-26 20:32:33 +0530

    Adding Coming Soon Page

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> Comming Soon... </h1>
+        <h3> Follow us on Instagram:
+		<a href="https://www.instagram.com/theworldofcorporate/">  @theworldofcorporate</a>
+		</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
